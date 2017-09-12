6d3bfb1ada0d9b40ea6b4fe92b85ab1f72b51475
Eugeny Karpov <michmanharitonov1@gmail.com>
2017-09-12 23:59:26 +0300

    add test jasper report

--- src/main/java/ru/highloadcup/dao/DefaultController.kt
+++ src/main/java/ru/highloadcup/dao/DefaultController.kt
@@ -6,15 +6,35 @@ import org.springframework.web.bind.annotation.RestController
 import ru.highloadcup.util.doAsync
 
 import javax.servlet.http.HttpServletRequest
+import net.sf.jasperreports.engine.JasperCompileManager
+import net.sf.jasperreports.engine.JasperExportManager
+import net.sf.jasperreports.engine.JasperFillManager
+import net.sf.jasperreports.engine.JasperReport
+import java.nio.file.Files
+import java.nio.file.Paths
+
 
 @RestController
 @RequestMapping("/hello")
 class DefaultController {
 
-    @GetMapping
+    /*@GetMapping
+    fun getDefault() = "it works"*/
+
+    /*@GetMapping
     fun getDefault(request: HttpServletRequest) {
         request.startAsync().doAsync { response ->
             response.writer.write("it works")
         }
+    }*/
+
+    @GetMapping
+    fun jasper(): ByteArray? {
+        val jasperReport = JasperCompileManager.compileReport("report.jrxml")
+        val parameters = hashMapOf("name" to "fieldValue")
+
+        val filledReport = JasperFillManager.fillReport(jasperReport, parameters as Map<String, Any>?)
+
+        return JasperExportManager.exportReportToPdf(filledReport)
     }
 }
